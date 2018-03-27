5f940202c89c965ee368bc5116e9e176f4c3afd7
askabukahfi <askabulkahfi1@gmail.com>
2018-03-27 18:34:16 +0800

    Mengisi nilai atribut

--- src/mobil/mobilaksi.java
+++ src/mobil/mobilaksi.java
@@ -15,6 +15,31 @@ public class mobilaksi {
         
         
         
-         
+        classmobil HRV = new classmobil();
+        System.out.println("HRV");
+        HRV.merk="honda";
+        HRV.harga=150000000;
+        HRV.jenis="SUV";
+        HRV.type="E";
+        HRV.warna="merah";
+        HRV.cetakinfo();
+        
+        classmobil brio = new classmobil();
+        System.out.println("brio");
+        brio.merk="honda";
+        brio.harga=100000000;
+        brio.jenis="Sedan";
+        brio.type="c";
+        brio.warna="merah";
+        brio.cetakinfo();
+        
+        classmobil jazz = new classmobil();
+        System.out.println("jazz");
+        jazz.merk="honda";
+        jazz.harga=120000000;
+        jazz.jenis="minicar";
+        jazz.type="A";
+        jazz.warna="putih";
+        jazz.cetakinfo();        
     }
 }
